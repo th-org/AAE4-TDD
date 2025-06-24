a6e13a6e57eb714b9b1dec1f558e617e7c55769b
Joao Igor <joaoigorpereira@gmail.com>
2025-06-24 20:38:52 -0300

    Refatorando o test_only_one.py

--- test_only_one.py
+++ test_only_one.py
@@ -2,11 +2,10 @@ import  string
 
 from GeradorDeSenhas import gerar_senha
 
-numeros_validos = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def test_only_one_character():
     senha = gerar_senha(8, True, True, True, True)
-    if not any(c in string.punctuation & c.islower() & c.isupper &  c in numeros_validos  for c in senha):
+    if not any(c in string.punctuation and string.ascii_lowercase and string.ascii_uppercase and  c in string.digits  for c in senha):
         print("A senha deve conter pelo menos um caractere de cada.")
         return False
     return True
