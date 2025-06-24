544a63cf498cc09eb1c2c82f73b189369b804d85
Yasm1nNasc1mento <yasmin.mn4@gmail.com>
2025-06-24 19:39:18 -0300

    adiciona test_allfalse.py

--- GeradorDeSenhas.py
+++ GeradorDeSenhas.py
@@ -1,27 +1,30 @@
 import random
 import string
 
+class senhaError(Exception):
+    pass
+
 def gerar_senha(tamanho, incluir_maiusculas, incluir_minusculas, incluir_numeros, incluir_simbolos):
+    
+    if not any([incluir_maiusculas, incluir_minusculas, incluir_numeros, incluir_simbolos]):
+        raise senhaError("Pelo menos um critério deve ser selecionado.")
+    
     caracteres = ""
-    try {
-        if incluir_maiusculas:
-            caracteres += string.ascii_uppercase
 
-        if incluir_minusculas:
-            caracteres += string.ascii_lowercase
+    if incluir_maiusculas:
+        caracteres += string.ascii_uppercase
 
-        if incluir_numeros:
-            caracteres += string.digits
+    if incluir_minusculas:
+        caracteres += string.ascii_lowercase
 
-        if incluir_simbolos:
-            caracteres += string.punctuation
+    if incluir_numeros:
+        caracteres += string.digits
 
-        senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
-        return senha
-    }catch{
-        if not any([self.tamanho, self.incluir_maiusculas, self.incluir_minusculas, self.incluir_numeros, self.incluir_simbolos]):
-            raise PasswordCriteriaError("Pelo menos um critério deve ser selecionado.")
-    }
+    if incluir_simbolos:
+        caracteres += string.punctuation
+
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
 
 
 if __name__ == "__main__":
--- test_allfalse.py
+++ test_allfalse.py
@@ -1,10 +1,10 @@
 import unittest
-from GeradorDeSenhas import gerar_senha
+from GeradorDeSenhas import gerar_senha, senhaError
 
 class GeradorDeSenhasTesteCase(unittest.TestCase):
     def test_all_false(self):
-        gerador = gerar_senha(tamanho=10, incluir_maiusculas=False, incluir_minusculas=False, incluir_simbolos=False)
-        gerador.gerador()
+        with self.assertRaises(senhaError):
+            gerar_senha(tamanho=10, incluir_maiusculas=False, incluir_minusculas=False, incluir_numeros=False, incluir_simbolos=False) 
 
 if __name__ == '__main__':
     unittest.main()
